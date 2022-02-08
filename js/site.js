9a1b3865b63af202f7809081e422315647323b71
Aide Oropeza Aguilar <36251470+aoropeza31@users.noreply.github.com>
2022-02-07 19:04:57 -0500

    updated app and code page
    I added a card to the app page and added the comments to my code page. Among other changes as well

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
-//call hello world
-function HelloWorld(){
-    alert("hello world");
+//get the string from the page
+//controller function
+function getString(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+}
+//reverse the string
+//logic function
+function reverseString(userString){
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length-1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+//display reversed string
+//view function
+function displayString(revString){
+    //write to page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
